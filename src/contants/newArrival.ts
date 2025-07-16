503c179c016c4569c5e8c3cacc0e2f0ffb893800
Ratu <ratukf@gmail.com>
2025-07-16 15:33:56 +0700

    feat: create new arrival constant for new arrival section in dashboard

---
+++ src/contants/newArrival.ts
@@ -0,0 +1,6 @@
+export const NEW_ARRIVAL = [
+    {label: 'Playstation 5', desc: 'Black and White version of the PS5 coming out on sale.', url: '/'},
+    {label: "Women's Collection", desc: 'Featured woman collections that give you another vibe.', url: '/'},
+    {label: 'Speakers', desc: 'Amazon wireless speakers', url: '/'},
+    {label: 'Perfume', desc: 'GUCCI INTENSE OUD EDP', url: '/'},
+]
