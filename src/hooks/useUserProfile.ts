941dbae0883c91a5af13ff6fd480768a611a3cc3
Ratu <ratukf@gmail.com>
2026-02-15 01:07:45 +0700

    feat(hooks): add update user profile hook & remove unused code

--- src/hooks/useUserProfile.ts
+++ src/hooks/useUserProfile.ts
@@ -1,57 +1,24 @@
-import { useState } from 'react';
-import { useDispatch } from 'react-redux';
-import { setAuth } from '../store/auth.slice';
-import type { Auth } from '../types/auth';
+import type { Profile } from '../types/userProfile';
+import { updateProfileAsyncAction } from '../store/userProfileAsyncAction';
+import { useAppDispatch } from '../store/hooks';
 
 export const useUserProfile = () => {
-  const dispatch = useDispatch();
-  // const [loading, setLoading] = useState(false);
-  const [loading] = useState(false);
+  const appDispatch = useAppDispatch();
 
-  // Set initial account values
-  const setInitialAccountValue = async (values: Auth) => {
+  // Update user profile
+  const handleUpdateUserProfile = async (id: string, profile: Profile) => {
     try {
-      dispatch(setAuth(values));
+      appDispatch(updateProfileAsyncAction({ id, profile }));
     } catch (error) {
-      console.error('Failed to set initial account values:', error);
+      console.error('Failed to update user profile', error);
     } finally {
-      console.log('Initial account values set');
+      console.log('Successfully update user profile');
     }
   };
 
-  // const editProfile = async (values: Profile) => {
-  //   setLoading(true);
-  //   try {
-  //     dispatch(setProfile(values));
-  //     showSnackBar?.('Profile is successfully edited', 'success');
-  //   } catch {
-  //     showSnackBar?.('Edit failed', 'error');
-  //   } finally {
-  //     setLoading(false);
-  //   }
-  // };
-
-  // const editAddressBooks = async (values: AddressBooks[]) => {
-  //   setLoading(true);
-  //   try {
-  //     dispatch(setAddressBook(values));
-  //     showSnackBar?.('Address Books are successfully edited', 'success');
-  //     console.log('euy');
-  //   } catch {
-  //     showSnackBar?.('Edit failed', 'error');
-  //   } finally {
-  //     setLoading(false);
-  //   }
-  // };
-
   return {
-    editAccount: {
-      loading,
-      // editProfile,
-      // editAddressBooks,
-    },
-    initialValue: {
-      setInitialAccountValue,
+    useUpdateUserProfile: {
+      updateUserProfile: handleUpdateUserProfile,
     },
   };
 };
